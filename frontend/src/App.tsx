eea6f065031bac5df1d579bee9b03b615b0b422f
Daniel Giles <mrgiles1@gmail.com>
2023-08-22 13:44:00 +0200

    saving login compenent to work on after Chakra ticket

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-//import logo from './logo.svg';
 import { ChakraProvider } from '@chakra-ui/react'
 import Login from './components/login';
-
 import './App.css';
 
-
 function App() {
-  // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
       <Login />
