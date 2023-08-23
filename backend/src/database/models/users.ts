8197579b770e19c5c770d091ac6a53a84666db12
noah milliken <noahmilliken@gmail.com>
2023-08-23 10:12:52 -0400

    CDW-16 fix arrow function syntax consistency remove console.logs fix naming convention fix .env import

--- backend/src/database/models/users.ts
+++ backend/src/database/models/users.ts
@@ -1,16 +1,13 @@
 const { MongoClient } = require("mongodb");
+const dotenv = require('dotenv')
 
-async function main() {
-  const uri = `mongodb+srv://noahmilliken:creamfilling@oreocluster.kasbgor.mongodb.net/?retryWrites=true&w=majority`;
+const connectDatabase = async () => {
 
-  const client = new MongoClient(uri);
+  const client = new MongoClient(URI);
 
   try {
     await client.connect();
 
-    await deleteUsersWithLongPasswords(client, "_");
-
-    await listDatabases(client);
   } catch (e) {
     console.error(e);
   } finally {
@@ -18,84 +15,52 @@ async function main() {
   }
 }
 
-main().catch(console.error);
+connectDatabase()
 
-async function deleteUsersWithLongPasswords(client, param) {
-  console.log(param);
+const deleteUsersWithLongPasswords = async(client, param) =>  {
   const result = await client
     .db("userDatabase")
     .collection("users")
     .deleteMany({ userName: { $regex: param } });
-  console.log(`${result.deletedCount} document(s) was/were deleted`);
 }
 
-async function deleteListingByName(client, userName: string) {
+const  deleteListingByName = async (client, userName: string) => {
   const result = await client
     .db("userDatabase")
     .collection("users")
     .deleteOne({ name: userName });
-
-  console.log(`${result.deletedCount} document(s) was/were deleted`);
 }
 
-async function upsertListingByName(client, nameOfUser, updatedUser) {
+const upsertListingByName = async (client, nameOfUser, updatedUser) => {
   const result = await client
     .db("userDatabase")
     .collection("users")
     .updateOne({ name: nameOfUser }, { $set: updatedUser }, { upsert: true });
-  console.log(`${result.matchedCount} document(s) matchet the query  criteria`);
-  if (result.upsertedCount) {
-    console.log(`${result.upsertedCount} documents were upserted.`);
-  }
-  console.log(`${result.modifiedCount} document was/were updated.`);
 }
-
-async function updateUserByName(client, nameOfUser, updatedUser) {
+const updateUserByName = async(client, nameOfUser, updatedUser) => {
   const result = await client
     .db("userDatabase")
     .collection("users")
     .updateOne({ name: nameOfUser }, { $set: updatedUser });
-  console.log(`${result.matchedCount} document(s) matched the criteria`);
-  console.log(`${result.modifiedCount} document was/were updated.`);
 }
 
-async function findOneUserByName(client, nameOfUser) {
+const findOneUserByName = async(client, nameOfUser) => {
   const result = await client
     .db("userDatabase")
     .collection("users")
     .findOne({ name: nameOfUser });
-
-  if (result) {
-    console.log(`found user with the name ${nameOfUser}`);
-    console.log(result);
-  } else {
-    console.log(`No listings found with the name ${nameOfUser}`);
-  }
 }
 
-async function createMultipleUsers(client, newUser) {
+const createMultipleUsers = async(client, newUser) => {
   const result = await client
     .db("userDatabase")
     .collection("users")
     .insertMany(newUser);
-  console.log(
-    `${result.insertedCount} new users created with id(s): ${result.insertedIs}`
-  );
 }
 
-async function createUser(client, newUser) {
+const createUser = async(client, newUser) => {
   const result = await client
     .db("userDatabase")
     .collection("users")
     .insertOne(newUser);
-
-  console.log(`New user created with id: ${result.insertedId}`);
-}
-
-async function listDatabases(client) {
-  const databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:");
-  databasesList.databases.forEach((db) => {
-    console.log(`_ ${db.name}`);
-  });
 }
