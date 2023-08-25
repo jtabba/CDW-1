85947ed8bd6edd478fbabae46a0c74aa990dd8a1
Corrie Stroup <Corrie.Stroup@gmail.com>
2023-08-25 19:54:31 +0700

    Basic routes implemented
    Implemented basic routes for home, profile, sign in, sign up, and 404. Need components/pages for render for each.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,26 +1,26 @@
-import { Avatar, Flex } from '@chakra-ui/react';
-import Logo from './Logo';
-import AuthButtons from './AuthButtons';
-import { FC } from 'react';
+import { Avatar, Flex } from "@chakra-ui/react";
+import Logo from "./Logo";
+import AuthButtons from "./AuthButtons";
+import { FC } from "react";
 //import NavLinks from './NavLinks';
 
 interface NavBarProps {
-    isLoggedIn: boolean,
+  isLoggedIn: boolean;
 }
 
-const NavBar: FC<NavBarProps> = ({isLoggedIn}) => {
-    return (
-        <Flex 
-            justifyContent='space-between'
-            alignItems='center'
-            h='60px'
-            outline={1}
-        >
-            <Logo />
-            {/*isLoggedIn ? <NavLinks /> : null */}
-            {isLoggedIn ? <Avatar /> : <AuthButtons />}
-        </Flex>
-    )
+const NavBar: FC<NavBarProps> = ({ isLoggedIn }) => {
+  return (
+    <Flex
+      justifyContent="space-between"
+      alignItems="center"
+      h="60px"
+      outline={1}
+    >
+      <Logo />
+      {/*isLoggedIn ? <NavLinks /> : null */}
+      {isLoggedIn ? <Avatar /> : <AuthButtons />}
+    </Flex>
+  );
 };
 
 export default NavBar;
