3031c48d3597923084cdd0c8297d0ea2322a5aba
jt <jtabba@hotmail.com>
2023-08-27 14:51:12 +1000

    Refactors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,68 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme/theme";
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
-import { useAuth0 } from "@auth0/auth0-react";
 import Home from "./pages/Home";
-import { getAllUsers } from "./requests/getAllUsers";
-import { userData } from "./requests/types";
 
 function App() {
-	const { isLoading, error } = useAuth0();
-	const [users, setUsers] = useState<userData[]>([]);
-
 	return (
 		<ChakraProvider theme={theme}>
-			<main className="App">
-				<h1>Auth0 Login</h1>
-				{error && <p>Authentication Error</p>}
-				{!error && isLoading && <p>Loading...</p>}
-				{!error && !isLoading && (
-					<>
-						<LoginButton />
-						<LogoutButton />
-						<Profile />
-					</>
-				)}
-			</main>
 			<Home />
-		</ChakraProvider>
-	);
-	useEffect(() => {
-		// request demo
-		// open browser console to see result
-		const getUsers = async () => {
-			const response = await getAllUsers();
-			console.log("response", response);
-
-			if (response.success) {
-				setUsers(response.data as userData[]);
-			} else {
-				// create error notification wth Chakra
-			}
-		};
-		getUsers();
-	}, []);
-
-	return (
-		<ChakraProvider theme={theme}>
-			<main className="App">
-				<h1>Auth0 Login</h1>
-				{error && <p>Authentication Error</p>}
-				{!error && isLoading && <p>Loading...</p>}
-				{!error && !isLoading && (
-					<>
-						<LoginButton />
-						<LogoutButton />
-						<Profile />
-						{users.map((user) => (
-							<Box>{user.name}</Box>
-						))}
-					</>
-				)}
-			</main>
 		</ChakraProvider>
 	);
 }
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,47 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "../components/LoginButton";
+import LogoutButton from "../components/LogoutButton";
+import Profile from "../components/Profile";
+import { useEffect, useState } from "react";
+import { userData } from "../requests/types";
+import { getAllUsers } from "../requests/getAllUsers";
+import { Box, Stack } from "@chakra-ui/react";
+
+export const Login = () => {
+	const { isLoading, error } = useAuth0();
+	const [users, setUsers] = useState<userData[]>([]);
+
+	useEffect(() => {
+		// request demo
+		// open browser console to see result
+		const getUsers = async () => {
+			const response = await getAllUsers();
+			console.log("response", response);
+
+			if (response.success) {
+				setUsers(response.data as userData[]);
+			} else {
+				// create error notification wth Chakra
+			}
+		};
+		getUsers();
+	}, []);
+
+	return (
+		<Stack direction={"row"} className="App">
+			<h1>Auth0 Login</h1>
+			{error && <p>Authentication Error</p>}
+			{!error && isLoading && <p>Loading...</p>}
+			{!error && !isLoading && (
+				<>
+					<LoginButton />
+					<LogoutButton />
+					<Profile />
+					{users.map((user) => (
+						<Box>{user.name}</Box>
+					))}
+				</>
+			)}
+		</Stack>
+	);
+};
