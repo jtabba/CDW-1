5b3d78f05b9c0ebd4173b8a52ca17a4266068ae2
Tim Chang <tim.chang87@gmail.com>
2023-08-28 10:08:34 -0400

    Rebase prod

--- backend/src/database/userDatabase.ts
+++ backend/src/database/userDatabase.ts
@@ -1,1 +1,11 @@
+import { connectToDb } from './db'
+const getAllUserData = async () => {
+    const collection = await connectToDb('Users')
+    return collection?.find({}).toArray()
+}
+const createUser = async (userData: object) => {
+    const collection = await connectToDb('Users')
+    await collection?.insertOne(userData)
+}
 
+export default { getAllUserData, createUser }
