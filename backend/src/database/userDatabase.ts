239a5f730613cbdb0d2eb56576c101234c0cb111
Tim Chang <tim.chang87@gmail.com>
2023-08-28 10:08:34 -0400

    Pull prod

--- backend/src/database/userDatabase.ts
+++ backend/src/database/userDatabase.ts
@@ -1,12 +1,11 @@
-import { connectToDb } from "./db"
+import { connectToDb } from './db'
 const getAllUserData = async () => {
     const collection = await connectToDb('Users')
     return collection?.find({}).toArray()
 }
-const createUser = async(userData: object) => {
+const createUser = async (userData: object) => {
     const collection = await connectToDb('Users')
     await collection?.insertOne(userData)
-    
 }
 
 export default { getAllUserData, createUser }
