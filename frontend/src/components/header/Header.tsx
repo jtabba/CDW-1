45a091176ed153c761de986cdc91b3781c65b107
Corrie Stroup <Corrie.Stroup@gmail.com>
2023-08-29 21:44:08 +0700

    Basic structure
    Basic structure of navbar that hides on mobile. Need to implement mobile nav

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -2,23 +2,24 @@ import { Flex } from "@chakra-ui/react";
 import Logo from "./Logo";
 import { FC } from "react";
 import { AuthButtons } from "../authentication/SignInOrOut";
-import { NavBar } from "./NavBar";
+// import { NavBar } from "./NavBar";
+import NavBar from "./NavBar";
 
 interface HeaderProps {}
 
 export const Header: FC<HeaderProps> = () => {
-	return (
-		<Flex
-			justifyContent="space-between"
-			alignItems="center"
-			h="60px"
-			outline={1}
-		>
-			<Logo />
+  return (
+    <Flex
+      justifyContent="space-between"
+      alignItems="center"
+      h="60px"
+      outline={1}
+    >
+      <Logo />
 
-			<NavBar />
+      <NavBar />
 
-			<AuthButtons />
-		</Flex>
-	);
+      <AuthButtons />
+    </Flex>
+  );
 };
