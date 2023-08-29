dbf6fad96c8ae31fbfe6527334e91819c0835bd5
noah milliken <noahmilliken@gmail.com>
2023-08-29 09:56:02 -0400

    CDW 30 Feature synced Pr veview changes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,18 +8,19 @@ import { Auth0ProviderWithNavigate } from "./components/authentication/Auth0Prov
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+	document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Auth0ProviderWithNavigate>
-          <App />
-        </Auth0ProviderWithNavigate>
-      </BrowserRouter>
-    </ChakraProvider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<ChakraProvider theme={theme}>
+			<BrowserRouter>
+				<Auth0ProviderWithNavigate>
+					<App />
+				</Auth0ProviderWithNavigate>
+			</BrowserRouter>
+		</ChakraProvider>
+	</React.StrictMode>
 );
