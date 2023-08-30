2b55ed3a1c1dee96e3d47b5234bb2651e03e6c1f
Daniel Giles <mrgiles1@gmail.com>
2023-08-30 07:43:27 +0200

    saving some changes to logo it navbar, investigating node module error in App.tsx

--- frontend/src/components/header/Logo.tsx
+++ frontend/src/components/header/Logo.tsx
@@ -1,10 +1,18 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image, Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo-white.png";
+
+
 
 const Logo = () => {
 	return (
-		<Flex className="logo">
-			<Link to={"home"}>MentorShip</Link>
+		<Flex className="logo" >
+			<Box display="flex" alignItems="center" justifyContent="center" border="1px" boxSize="" p={10} w="100%">			
+				<Image objectFit="cover" boxSize="75px" src={logo} />
+				<Link to={"home"}>MentorShip</Link>
+			</Box>
+			{/* <img src={logo} alt="logo" /> */}
+			{/* <Link to={"home"}>MentorShip</Link> */}
 		</Flex>
 	);
 };
