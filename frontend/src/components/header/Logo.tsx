f5627b4e6348aab9b375de206a69431d59de999a
Daniel Giles <mrgiles1@gmail.com>
2023-08-30 12:05:35 +0200

    added logo to Hero.tsx, edited headline for responsiveness testing

--- frontend/src/components/header/Logo.tsx
+++ frontend/src/components/header/Logo.tsx
@@ -1,6 +1,6 @@
 import { Flex, Image, Box, Heading} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo-white.png";
+import logo from "../../assets/logo-white-blue-round.png";
 import '@fontsource/bree-serif';
 // import './Logo.css';
 
@@ -10,7 +10,7 @@ const Logo = () => {
 	return (
 		<Flex>
 			<Box className="logo" display="flex" alignItems="center" justifyContent="flex-start" pr={10} w="100%">			
-				<Image mr="15px" objectFit="cover" boxSize="38px" src={logo} />
+				<Image mr="15px" objectFit="cover" boxSize="50px" src={logo} />
 				<Heading><Link to={"home"}>MentorShip</Link></Heading>
 			</Box>
 		</Flex>
