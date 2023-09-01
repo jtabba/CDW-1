44a735e14c5e5958981a39a68fecf6dca0ff748c
Daniel Giles <mrgiles1@gmail.com>
2023-09-01 11:12:36 +0200

    added some new text, still more to add

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -5,6 +5,14 @@ import FeatureSection from "../components/FeatureSection";
 import SubFeatureSection from "../components/SubFeatureSection";
 import MetricSection from "../components/MetricSection";
 
+
+interface SubFeatureProps {
+	headline: string;
+	content: string | JSX.Element; // Updated type to allow both string and JSX.Element
+	linkLabel: string;
+	url: string;
+}
+
 const Home = () => {
 	//Props stored here until we start implementing an appropriate store
 	const heroProps = {
@@ -16,25 +24,24 @@ const Home = () => {
 	};
 
 	const featureProps = {
-		headline: "Lorem Ipsum",
+		headline: "Find a mentor and get ready to sail",
 		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+			"Whether you're navigating a career transition, striving to elevate your developer prowess, or seeking enhanced efficiency in your programming journey, we're your catalyst for success. Our platform serves as the bridge, connecting you with seasoned developers and accomplished engineers. Together, we unlock the power of mentorship and pave the way for more effective, empowered learning experiences.",
 		buttonLabel: "Lorem Ipsum",
 		buttonUrl: "/loremipsum"
 	};
 
 	const subFeaturesProps = [
 		{
-			headline: "Lorem Ipsum 1",
+			headline: "Mentorship that Guides, Learning that Thrives",
 			content:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+				"Programming can seem overwhelming, given the abundance of learning materials, and finding a reliable mentor isn't always straightforward. At MentorShip, our goal is to make learning and improvement accessible to all. We achieve this by matching mentees with mentors who suit their specific needs. Our aim is to empower individuals from diverse locations, helping them enhance their programming skills and confidently embark on their professional journey.",
 			linkLabel: "Lorem Ipsum 1 >",
 			url: "/loremipsum1"
 		},
 		{
-			headline: "Lorem Ipsum 2",
-			content:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+			headline: "How MentorShip Works:",
+			content:"trying to set this as a jsx element",
 			linkLabel: "Lorem Ipsum 2 >",
 			url: "/loremipsum2"
 		},
