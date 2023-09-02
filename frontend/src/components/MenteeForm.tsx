8da8b95c822c8dacd1eca9b879e32524e5c1091b
Tim Chang <tim.chang87@gmail.com>
2023-09-02 10:32:57 -0400

    Optimize types for onboarding form.

--- frontend/src/components/MenteeForm.tsx
+++ frontend/src/components/MenteeForm.tsx
@@ -3,75 +3,56 @@ import FormContainer from './FormContainer';
 import FormRow from './FormRow';
 import { Flex, InputGroup, Button } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
+import { Field, MenteeInfo } from '../types';
 
-interface Field {
-    inputType: string;
-    label: string;
-    name: string;
-}
+const MenteeForm: FC<{ menteeFields: Field[] }> = ({ menteeFields }) => {
+  const navigate = useNavigate();
 
-interface MenteeFields {
-    menteeFields: Field[] 
-}
+  let [menteeInfo, setMenteeInfo] = useState<MenteeInfo>({
+    role: 'mentee',
+    bio: '',
+    learningGoals: '',
+    experience: '',
+    jobTitle: '',
+    interests: '',
+  });
 
-interface MenteeInfo {
-    role: string;
-    bio: string;
-    learningGoals: string;
-    experience: string;
-    jobTitle: string;
-    interests: string;
-  }
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => setMenteeInfo({ ...menteeInfo, [e.target.name]: e.target.value });
 
-const MenteeForm: FC<MenteeFields> = ( { menteeFields }) => {
-    const navigate = useNavigate();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    //POST request logic goes here
+    e.preventDefault();
+    navigate('/home');
+  };
 
-    let [menteeInfo, setMenteeInfo] = useState<MenteeInfo>({
-        role: 'mentee',
-        bio: '',
-        learningGoals: '',
-        experience: '',
-        jobTitle: '',
-        interests: '',
-      });
-
-      const handleChange = (
-        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-      ) => setMenteeInfo({ ...menteeInfo, [e.target.name]: e.target.value });
-    
-      const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        //POST request logic goes here
-        e.preventDefault();
-        navigate('/home');
-      };
-
-    return (
-        <FormContainer heading='Mentee'>
-          <form onSubmit={handleSubmit}>
-            <Flex flexDirection='column' gap={10}>
-              <InputGroup flexDirection='column' gap={10}>
-                {menteeFields.map((field: Field, index: number) => (
-                  <FormRow
-                    key={index}
-                    inputType={field.inputType}
-                    label={field.label}
-                    name={field.name}
-                    flexDirection='column'
-                    fontSize='clamp(1.3rem, -0.875rem + 8.333vw, 1rem)'
-                    value={menteeInfo[field.name as keyof MenteeInfo]}
-                    onChange={handleChange}
-                  />
-                ))}
-              </InputGroup>
-            </Flex>
-            <br />
-            <Button size='lg' type='submit'>
-              Submit
-            </Button>
-          </form>
-        </FormContainer>
-    );
-
+  return (
+    <FormContainer heading='Mentee'>
+      <form onSubmit={handleSubmit}>
+        <Flex flexDirection='column' gap={10}>
+          <InputGroup flexDirection='column' gap={10}>
+            {menteeFields.map((field: Field, index: number) => (
+              <FormRow
+                key={index}
+                inputType={field.inputType}
+                label={field.label}
+                name={field.name}
+                flexDirection='column'
+                fontSize='clamp(1.3rem, -0.875rem + 8.333vw, 1rem)'
+                value={menteeInfo[field.name as keyof MenteeInfo]}
+                onChange={handleChange}
+              />
+            ))}
+          </InputGroup>
+        </Flex>
+        <br />
+        <Button size='lg' type='submit'>
+          Submit
+        </Button>
+      </form>
+    </FormContainer>
+  );
 };
 
 export default MenteeForm;
--- frontend/src/components/MentorForm.tsx
+++ frontend/src/components/MentorForm.tsx
@@ -3,77 +3,57 @@ import FormContainer from './FormContainer';
 import FormRow from './FormRow';
 import { Flex, InputGroup, Button } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
+import { Field, MentorInfo } from '../types';
 
-interface Field {
-    inputType: string;
-    label: string;
-    name: string;
-}
+const MentorForm: FC<{ mentorFields: Field[] }> = ({ mentorFields }) => {
+  const navigate = useNavigate();
 
-interface MentorFields {
-    mentorFields: Field[] 
-}
+  let [mentorInfo, setMentorInfo] = useState<MentorInfo>({
+    role: 'mentor',
+    bio: '',
+    expertise: '',
+    experience: '',
+    jobTitle: '',
+    interests: '',
+    hourlyRate: '',
+  });
 
-interface MentorInfo {
-    role: string;
-    bio: string;
-    expertise: string;
-    experience: string;
-    jobTitle: string;
-    interests: string;
-    hourlyRate: string;
-  }
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => setMentorInfo({ ...mentorInfo, [e.target.name]: e.target.value });
 
-const MentorForm: FC<MentorFields> = ( { mentorFields }) => {
-    const navigate = useNavigate();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    //POST request logic goes here
+    e.preventDefault();
+    navigate('/home');
+  };
 
-    let [mentorInfo, setMentorInfo] = useState<MentorInfo>({
-        role: 'mentor',
-        bio: '',
-        expertise: '',
-        experience: '',
-        jobTitle: '',
-        interests: '',
-        hourlyRate: '',
-      });
-
-      const handleChange = (
-        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-      ) => setMentorInfo({ ...mentorInfo, [e.target.name]: e.target.value });
-    
-      const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        //POST request logic goes here
-        e.preventDefault();
-        navigate('/home');
-      };
-
-    return (
-        <FormContainer heading='Mentor'>
-          <form onSubmit={handleSubmit}>
-            <Flex flexDirection='column' gap={10}>
-              <InputGroup flexDirection='column' gap={10}>
-                {mentorFields.map((field: Field, index: number) => (
-                  <FormRow
-                    key={index}
-                    inputType={field.inputType}
-                    label={field.label}
-                    name={field.name}
-                    flexDirection='column'
-                    fontSize='clamp(1.3rem, -0.875rem + 8.333vw, 1rem)'
-                    value={mentorInfo[field.name as keyof MentorInfo]}
-                    onChange={handleChange}
-                  />
-                ))}
-              </InputGroup>
-            </Flex>
-            <br />
-            <Button size='lg' type='submit'>
-              Submit
-            </Button>
-          </form>
-        </FormContainer>
-    );
-
+  return (
+    <FormContainer heading='Mentor'>
+      <form onSubmit={handleSubmit}>
+        <Flex flexDirection='column' gap={10}>
+          <InputGroup flexDirection='column' gap={10}>
+            {mentorFields.map((field: Field, index: number) => (
+              <FormRow
+                key={index}
+                inputType={field.inputType}
+                label={field.label}
+                name={field.name}
+                flexDirection='column'
+                fontSize='clamp(1.3rem, -0.875rem + 8.333vw, 1rem)'
+                value={mentorInfo[field.name as keyof MentorInfo]}
+                onChange={handleChange}
+              />
+            ))}
+          </InputGroup>
+        </Flex>
+        <br />
+        <Button size='lg' type='submit'>
+          Submit
+        </Button>
+      </form>
+    </FormContainer>
+  );
 };
 
 export default MentorForm;
