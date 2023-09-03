e01f8f1b298615ac133dc9696c79e1020bb8c094
Daniel Giles <mrgiles1@gmail.com>
2023-09-03 10:48:21 +0200

    finished text in MetricSection, imported color from theme, fixed minor logo issue

--- frontend/src/components/MetricSection.tsx
+++ frontend/src/components/MetricSection.tsx
@@ -1,6 +1,7 @@
 import Metric from "./Metric";
-import { Card } from '@chakra-ui/react';
+import { Card , Box, Heading, Flex, Text, Button, Image } from '@chakra-ui/react';
 import { FC } from 'react';
+import { themeBlue } from "../theme/theme";
 
 interface MetricsProps {
     statNumber: string
@@ -15,14 +16,30 @@ interface MetricsProps {
 const MetricSection: FC<Props> = ({ metricsProps }) : JSX.Element => {
 
     return (
-        <Card
-            flexDirection='row'
-            justifyContent='space-evenly'
-            p='15px'
-            width='65%'
-        >
-            {metricsProps.map((metric, index) => <Metric key={metric.id} statNumber={metric.statNumber} statText={metric.statText}/> )}
-        </Card>
+        // KEEP COMMENTED CODE BELOW IN ORDER TO REVERT TO USE TEXT FROM HOME.TSX, UPON DIRECTION FROM @JT
+        // <Card
+        //     flexDirection='row'
+        //     justifyContent='space-evenly'
+        //     p='15px'
+        //     width='65%'
+        // >
+        //     {metricsProps.map((metric, index) => <Metric key={metric.id} statNumber={metric.statNumber} statText={metric.statText}/> )}
+        // </Card>
+    <Box maxWidth={{ base: '90%', md: '90%' }}>
+        <Heading fontSize={{ base: '2xl', md: '4xl' }} mb={4}>
+                Our Mission
+        </Heading>
+        <Flex flexDirection={{ base: 'column', md: 'row' }} alignItems='center'>
+            <Box width={{ base: '100%', md: '100%' }}>
+                <Text fontSize={{ base: 'lg', md: 'xl'}} pt={2} pr={2}>
+                At MentorShip, our mission is to reshape the landscape of software development mentorship. We're committed to providing an innovative platform that empowers senior developers to guide and mentor pre-junior developers, fostering an environment of collaborative learning and growth. Through our maritime-inspired approach, we aim to create a community where knowledge flows freely, challenges are met with enthusiasm, and connections are forged that last a lifetime. Our goal is to revolutionize how mentorship takes shape, enabling aspiring developers to chart their course towards excellence while sailing alongside seasoned experts. Join us on this transformative journey, where mentorship knows no limits and the waves of coding mastery are yours to conquer.
+                </Text>
+                <Button color={themeBlue} mt={6} mb={4} variant='outline'>
+                    Join Today!
+                </Button>
+            </Box>
+        </Flex>
+    </Box>
     );
 };
 
