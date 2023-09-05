609f8dcd8a93a7a05fe080102ff772432ae74c64
Corrie Stroup <Corrie.Stroup@gmail.com>
2023-09-05 19:49:56 +0700

    Fixed spacing between header, body, and footer

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export const UserInterface = () => {
   return (
-    <Container margin="auto" maxWidth="90%" padding={"28px 0px"} pt={20}>
+    <Container marginX={"auto"} maxWidth="90%" padding={"28px 0px"}>
       <Grid templateRows="repeat(3, auto)" gap={1}>
         <Header />
 
