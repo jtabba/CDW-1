c98a7fef36366d81d8575020ef8d1b3a2888bd42
Corrie Stroup <Corrie.Stroup@gmail.com>
2023-09-07 20:59:10 +0700

    Slimmed down footer code

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,29 +1,12 @@
 import { Flex, Box, Text, Link, Divider } from "@chakra-ui/react";
 
 const Footer = () => {
-  const footerProps: { page: string; url: string }[] = [
+  const footerLinks: { page: string; url: string }[] = [
     { page: "Help", url: "/help" },
     { page: "Propsective Mentors", url: "/mentors" },
     { page: "Terms", url: "/terms" },
     { page: "Privacy Policy", url: "/privacy" },
   ];
-
-  const links: JSX.Element[] = [];
-
-  footerProps.forEach((element, index) => {
-    links.push(
-      <Link key={element.page} fontSize="10px" href={element.url}>
-        {element.page}
-      </Link>
-    );
-    if (index !== footerProps.length - 1)
-      links.push(
-        <Text key={element.page + "div"} fontSize="10px">
-          {" "}
-          |{" "}
-        </Text>
-      );
-  });
 
   return (
     <Box flexWrap="wrap" height="10vh">
@@ -31,15 +14,14 @@ const Footer = () => {
       <Flex justifyContent="space-between">
         <Text fontSize="10px">Copyright © 2023 Lorem Ipsum</Text>
         <Flex gap="5px">
-          {footerProps.map((element, index) => {
+          {footerLinks.map((element, index) => {
             return (
               <>
                 <Link key={element.page} fontSize="10px" href={element.url}>
                   {element.page}
                 </Link>
                 <Text key={element.page + "div"} fontSize="10px">
-                  {" "}
-                  {index !== footerProps.length - 1 ? "|" : ""}
+                  {index !== footerLinks.length - 1 ? "|" : ""}
                 </Text>
               </>
             );
